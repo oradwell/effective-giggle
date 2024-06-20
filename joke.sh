d020d67fa87586978c18b1fe4cbdc4638c4c9cd3
Oliver Radwell <oliver.radwell@gmail.com>
2024-06-20 21:20:17 +0100

    feat: add another joke

--- joke.sh
+++ joke.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Store the jokes in an array
-jokes=("I told my wife she should embrace her mistakes. She gave me a hug." "I'm reading a book on the history of glue. I just can't seem to put it down." "I told my computer I needed a break. Now it won't stop sending me Kit-Kats.")
+jokes=("I told my wife she should embrace her mistakes. She gave me a hug." "I'm reading a book on the history of glue. I just can't seem to put it down." "I told my computer I needed a break. Now it won't stop sending me Kit-Kats." "Why don't skeletons fight each other? They don't have the guts.")
 
-# Generate a random number between 0 and 2
-random_number=$((RANDOM % 3))
+# Generate a random number between 0 and 3
+random_number=$((RANDOM % 4))
 
 # Print out the joke at the random index
 echo "${jokes[$random_number]}"
